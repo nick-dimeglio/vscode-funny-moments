1f70516bee8877b1b2e38c016fe3afc5bcede385
Nicholas DiMeglio <ndimegl1@stevens.edu>
2023-04-28 20:22:31 -0400

    Modified code to fight boredom more effectively.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # On-Demand Family Guy Funny Moments for When You Code
 
-## To activate this extension use the `need more stimulation 💀💀💀💀` command from the command palette (`Ctrl + Shift + P` / `⇧ ⌘ P`)
+## To activate this extension use either the `need more stimulation under my code 💀💀💀💀` command or the `need more stimulation to the side 💀💀💀💀` from the command palette (`Ctrl + Shift + P` / `⇧ ⌘ P`)
 
-![VS Code Funny Moments](https://i.imgur.com/IAYr09o.png)
+## I stole a lot of this from https://github.com/jirkavrba/vscode-subway-surfers, and in honor of that, I have ensured that this also allows you to open a tab to the bottom of your viewport to allow you to watch subway surfers AND family guy funny moments at the same time. Coding has never been more entertaining.
+
+![VS Code Funny Moments](https://imgur.com/RDQmCl2.png)
--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import * as vscode from 'vscode';
 
 const videos = [
 	"Zxl28UgHpn0",
-	"mn-Tlb_wfjc",
+	"fytR78K6rHs",
 	"rROQPPTdQGs",
 	"i0mfOuMLykM"
 ];
@@ -16,11 +16,12 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('family-guy-funny-moments.funnies', () => {
-		const column = { viewColumn: vscode.ViewColumn.Beside, preserverFocus: true };
-		const options = { enableScripts: true };
+	let disposable = vscode.commands.registerCommand('family-guy-funny-moments.funniesBelow', () => {
+		const column = { viewColumn: vscode.ViewColumn.Beside};
+		const options = { enableScripts: true, splitView: true };
 
-		const panel = vscode.window.createWebviewPanel('funny-moments.video', "This code boring ah hell", column, options);
+		const panel = vscode.window.createWebviewPanel('funny-moments.video', "code boring asf 💀💀", column, options);
+        vscode.commands.executeCommand('workbench.action.moveActiveEditorGroupDown');
         let randomNum = Math.floor(Math.random() * 4);
 		const video = videos[randomNum];
 
@@ -43,7 +44,7 @@ export function activate(context: vscode.ExtensionContext) {
                 </head>
                 <body>
                     <div id="video">
-						<video autoplay muted controls width="300">
+						<video autoplay muted controls width="50%">
 							<source src="https://yewtu.be/latest_version?id=${video}&amp;itag=22#t=100">
 						</video>
 					</div>
@@ -53,6 +54,44 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+    let disposable2 = vscode.commands.registerCommand('family-guy-funny-moments.funniesSide', () => {
+		const column = { viewColumn: vscode.ViewColumn.Beside};
+		const options = { enableScripts: true, splitView: true };
+
+		const panel = vscode.window.createWebviewPanel('funny-moments.video', "code boring asf 💀💀", column, options);
+        let randomNum = Math.floor(Math.random() * 4);
+		const video = videos[randomNum];
+
+		panel.reveal();
+		panel.webview.html = `
+            <html lang="en"> 
+                <head>
+                    <meta charset="utf-8"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <style>
+                        #video {
+                            display: flex;
+                            flex-flow: column nowrap;
+                            justify-content: center;
+                            align-items: center;
+                            width: 100%;
+                            height: 100%;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <div id="video">
+						<video autoplay muted controls width="100%">
+							<source src="https://yewtu.be/latest_version?id=${video}&amp;itag=22#t=100">
+						</video>
+					</div>
+                </body>
+            </html>
+        `;
+	});
+
+	context.subscriptions.push(disposable2);
 }
 
 // This method is called when your extension is deactivated
